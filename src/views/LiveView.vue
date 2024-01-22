164ea277b31c4305e6c0333cabc44d0ff1e31f06
Tsaren <117976226+TsarenIBaren@users.noreply.github.com>
2024-01-22 11:20:49 +0200

    LUNCHTAJM

--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -3,12 +3,12 @@
 
 <template>
 <div class="content box" id="temp">
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
-    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck"></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XhV3Mhgi6Ck" allowfullscreen></iframe>
 <!--<iframe width="914" height="514" src="https://www.youtube.com/embed/XhV3Mhgi6Ck" title="Sheep Barn Cam at Farm Sanctuary powered by EXPLORE.org" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
 </div>
 </template>
@@ -22,6 +22,10 @@
 }
 
 iframe {
-    
+    border: 0.25em solid hsl(0, 0%, 100%);
+    border-radius: 0.5em;
+    margin: 0.5em;
+    width: 16em;
+    height: 9em;
 }
 </style>
